58275c0aa5e4490da8b704b9a758d03a9bb2c93a
Charles Dubout <charles@macbook-pro-de-charles-dubout.local>
2009-08-29 16:19:41 +0200

    first commit

---
+++ src/Test.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : Test.cpp
+// Author      : Charles Dubout
+// Version     : 0.2
+// Copyright   : All rights reserved.
+// Description : Hello World in C++, Ansi-style.
+// Revision    : Moved the using namespace declaration inside main.
+//============================================================================
+
+#include <iostream>
+
+int main() {
+	using namespace std;
+	cout << "Hello World!" << endl; // prints Hello World!
+	return 0;
+}
